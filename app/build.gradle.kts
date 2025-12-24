328183a3b4bc9b3ecb6d5b290da4422bcbbe35f2
Nick Sanft <nsanft@sanft.com>
2025-12-23 18:07:04 -0600

    Change SDK version, linting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.divora.toodo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.divora.toodo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
